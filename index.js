3f361edd75f0a5df550acb72dc1b6d9569ec5406
Espen K. Brun <espen_brun@hotmail.com>
2017-05-14 15:45:05 +0200

    Removed arrows in number field of form. also disable increment/decrement by arrow keys or mouse wheel

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
-	console.log('hello bilklubben');
-
+	// load footer and header
 	$('.navbar').load('navbar.html');
 	$('.footer').load('footer.html');
+
+	// Disable increment in number input by scrolling or arrow keys
+    // Disable scroll when focused on a number input.
+    $('form').on('focus', 'input[type=number]', function(e) {
+        $(this).on('wheel', function(e) {
+            e.preventDefault();
+        });
+    });
+    // Restore scroll on number inputs.
+    $('form').on('blur', 'input[type=number]', function(e) {
+        $(this).off('wheel');
+    });
+    // Disable up and down keys.
+    $('form').on('keydown', 'input[type=number]', function(e) {
+        if ( e.which == 38 || e.which == 40 )
+            e.preventDefault();
+    });  
+      
+
 
 });
